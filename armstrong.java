db7598c1221a47eb01115421a755d2ddacfd1f8f
Senguttuone <120941220+Senguttuone@users.noreply.github.com>
2022-12-24 11:06:25 +0530

    Update armstrong.java

--- armstrong.java
+++ armstrong.java
@@ -4,9 +4,7 @@ public class armstrong {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner obj=new Scanner(System.in);
-		System.out.println("Enter a number to find the number is ARMSTRONG no or NON ARMSTRONG no");
-		int num=obj.nextInt();
+		int num=323;
 		int refer=num;
 	int temp=0;
 		while(num>0)
